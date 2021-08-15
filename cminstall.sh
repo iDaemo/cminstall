b62c3686cfc7b59c529a00d87923dbe7d126353c
iDaemo <37049068+iDaemo@users.noreply.github.com>
2021-08-15 12:00:05 +0700

    Update cminstall.sh

--- cminstall.sh
+++ cminstall.sh
@@ -2,8 +2,8 @@
 mkdir -p /etc/centminmod
 touch /etc/centminmod/custom_config.inc
 EMAIL=nattapoom.p@gmail.com
-#echo "DISABLE_IPVSIX='y'" >> /etc/centminmod/custom_config.inc
-echo "PUSHOVER_EMAIL='nattapoom.p@gmail.com'" >> /etc/centminmod/custom_config.inc
+echo "DISABLE_IPVSIX='y'" >> /etc/centminmod/custom_config.inc
+#echo "PUSHOVER_EMAIL='nattapoom.p@gmail.com'" >> /etc/centminmod/custom_config.inc
 echo "ENABLEMOTD_CSFMSG='n'" >> /etc/centminmod/custom_config.inc # motd banner csf message alert disaply
 echo "ENABLEMOTD_LINKSMSG='n'" >> /etc/centminmod/custom_config.inc  # motd centminmod.com links bookmark alert display
 echo "FPM_WAKEUP_PATCH='y'" >> /etc/centminmod/custom_config.inc
@@ -12,18 +12,18 @@ echo "DEVTOOLSETSEVEN='n'" >> /etc/centminmod/custom_config.inc
 echo "DEVTOOLSETEIGHT='n'" >> /etc/centminmod/custom_config.inc
 echo "DEVTOOLSETNINE='y'" >> /etc/centminmod/custom_config.inc
 echo "NGINX_DEVTOOLSETGCC='y'" >> /etc/centminmod/custom_config.inc
+echo "CLANG='n'" /etc/centminmod/custom_config.inc
+echo "LIBRESSL_SWITCH='n'" >> /etc/centminmod/custom_config.inc
 #echo "MARCH_TARGETNATIVE_ALWAYS='y'" >> /etc/centminmod/custom_config.inc
 echo "NSD_INSTALL='n'" >> /etc/centminmod/custom_config.inc
 echo "NSD_DISABLED='y'" >> /etc/centminmod/custom_config.inc
-echo "NGINX_HPACK='y'" >> /etc/centminmod/custom_config.inc
+#echo "NGINX_HPACK='y'" >> /etc/centminmod/custom_config.inc
 echo "PUREFTPD_DISABLED='y'" >> /etc/centminmod/custom_config.inc
 echo "PUREFTPD_INSTALL='n'" >> /etc/centminmod/custom_config.inc
-echo "LIBRESSL_SWITCH='n'" >> /etc/centminmod/custom_config.inc
-echo "CLANG='n'" /etc/centminmod/custom_config.inc
-echo "CUSTOMSERVERSTRING='nginx'" >> /etc/centminmod/custom_config.inc
+#echo "CUSTOMSERVERSTRING='nginx'" >> /etc/centminmod/custom_config.inc
 echo "MARIADB_INSTALLTENFOUR='y'" >> /etc/centminmod/custom_config.inc #mariadb version allow
 echo "VHOSTCTRL_CLOUDFLAREINC='y'" >> /etc/centminmod/custom_config.inc
-echo "WPPLUGINS_ALL='n'" >> /etc/centminmod/custom_config.inc
+#echo "WPPLUGINS_ALL='n'" >> /etc/centminmod/custom_config.inc
 echo "CUSTOM_CURLRPM='y'" >> /etc/centminmod/custom_config.inc
 echo "CUSTOM_CURLRPMVER='7.69.0'" >> /etc/centminmod/custom_config.inc # custom curl/libcurl version
 echo "NGINX_IOURING_PATCH='y'" >> /etc/centminmod/custom_config.inc
@@ -32,7 +32,7 @@ echo "NGINX_STAPLE_CACHE_OVERRIDE='y'" >> /etc/centminmod/custom_config.inc
 echo "NGINX_STAPLE_CACHE_TTL='86400'" >> /etc/centminmod/custom_config.inc
 echo "NGINX_LIBBROTLI='y'" >> /etc/centminmod/custom_config.inc
 echo "NGXDYNAMIC_BROTLI='y'" >> /etc/centminmod/custom_config.inc
-echo "SET_DEFAULT_MYSQLCHARSET='utf8mb4'" >> /etc/centminmod/custom_config.inc
+#echo "SET_DEFAULT_MYSQLCHARSET='utf8mb4'" >> /etc/centminmod/custom_config.inc
 echo "AUTOHARDTUNE_NGINXBACKLOG='y'" >> /etc/centminmod/custom_config.inc
 echo "ZSTD_LOGROTATE_NGINX='y'" >> /etc/centminmod/custom_config.inc
 echo "ZSTD_LOGROTATE_PHPFPM='y'" >> /etc/centminmod/custom_config.inc
@@ -68,3 +68,13 @@ csf -ra
 sed -i "s|EMAIL=.*|EMAIL='$EMAIL'|" /etc/cron.daily/diskalert #set disk alert
 openssl dhparam -out /usr/local/nginx/conf/ssl/dhparam.pem 2048
 echo "Done!"
+
+###############################################################
+# centmin.sh menu option 22 specific options
+#WPPLUGINS_ALL='n'           # do not install additional plugins
+#WPCLI_CE_QUERYSTRING_INCLUDED='n' # https://community.centminmod.com/posts/85893/
+#WPCLI_SUPERCACHEPLUGIN='n'  # https://community.centminmod.com/threads/5102/
+# choose Cache Enabler 1.4.9 cache query string inclusion set to 'y'
+# choose Cache Enabler 1.5.1 cache query string exclusion set to 'n'
+#CACHE_ENABLER_LEGACY_CACHE='y'
+###############################################################
